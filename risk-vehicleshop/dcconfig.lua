cad5b26c22562894b542748b1f918361a2825cc0
now8018-droid <now8018@gmail.com>
2026-03-29 16:18:33 +0700

    Delete risk-vehicleshop directory

--- risk-vehicleshop/dcconfig.lua
+++
@@ -1,9 +0,0 @@
-    ConfigDC = {
-
-    enable          = false,
-    webhook         = "https://discord.com/api/webhooks/XXXXXXXX",
-    username        = "RISK VEHICLESHOP",
-    avatar          = "https://i.postimg.cc/NMKV9pRn/logo.png",
-    color           = 16777045,
-    log_purchase    = true
-}
--- risk-vehicleshop/risk-vehicleshop.sql
+++
@@ -1,6 +0,0 @@
--- risk-vehicleshop SQL migration
--- Purpose: ensure ESX owned_vehicles has `spawnname` column used by this resource.
-
-ALTER TABLE `owned_vehicles`
-ADD COLUMN IF NOT EXISTS `spawnname` VARCHAR(64) NULL DEFAULT NULL AFTER `job`;
-
